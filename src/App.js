d8816d999902d51cf5f7edcfb9894508ba18b8cf
Lalit Choudhary <137052617+lalitjakhar@users.noreply.github.com>
2024-09-13 11:17:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./Redux/Product/store";
 import Imggallery from "./Component/Imggallery";
-import Product from "./Component/Product";
+import Product from "./Component/ProductContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Product />
+        <ProductContainer />
         <Imggallery />
       </div>
     </Provider>
